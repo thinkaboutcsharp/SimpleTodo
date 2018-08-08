71bba36e4e94cf28a79b5244b1c16ac649600dc2
Ryu <thinkaboutcsharp@yahoo.co.jp>
2018-08-08 17:23:51 +0900

    だいぶ形になってきた。 細かく作り込む前に、名前を何とかしよう。

---
+++ SimpleTodo.Android/FitMasterDetailPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SimpleTodo.Droid
+{
+    public class FitMasterDetailPage : ContentPage
+    {
+        public FitMasterDetailPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ SimpleTodo.iOS/FitMasterDetailPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SimpleTodo.iOS
+{
+    public class FitMasterDetailPage : ContentPage
+    {
+        public FitMasterDetailPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
