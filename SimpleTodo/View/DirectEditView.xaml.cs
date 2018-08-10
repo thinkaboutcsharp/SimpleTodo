527e3206d5cd6e6ba560b07a2af43894e998d198
Ryu <thinkaboutcsharp@yahoo.co.jp>
2018-08-11 00:25:48 +0900

    まあまあ動いてきているがObservableCollectionとItemsSourceの相性がうまくいかず、苦戦中 とりあえずこの状態を保存

---
+++ SimpleTodo/View/DirectEditView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SimpleTodo
+{
+    public partial class DirectEditView : ContentView
+    {
+        public DirectEditView()
+        {
+            InitializeComponent();
+        }
+    }
+}
